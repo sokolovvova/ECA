14574ee04d58ce354e69fdba0094821193032919
Vladimir <vova.sokolov.1997@gmail.com>
2020-11-02 14:57:57 +0300

    - useless dep.

--- app/src/main/java/com/svadev/eca/fragments/ContractListFragment.kt
+++ app/src/main/java/com/svadev/eca/fragments/ContractListFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,7 +12,6 @@ import com.svadev.eca.MainActivity
 import com.svadev.eca.MainViewModel
 import com.svadev.eca.adapters.CLAdapter
 import com.svadev.eca.R
-import com.svadev.eca.models.ContractModel
 import kotlinx.android.synthetic.main.contract_list_fragment.view.*
 
 class ContractListFragment: Fragment(),CLAdapter.ContractOnClickListener {
